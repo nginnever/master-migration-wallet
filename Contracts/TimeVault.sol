fde69bbf8a50a79a14b2a77c054eb9892e5413eb
nginnever <ginneversource@gmail.com>
2017-05-08 16:26:52 -0700

    vault update

--- Contracts/TimeVault.sol
+++ Contracts/TimeVault.sol
@@ -1,25 +1,20 @@
-pragma solidity ^0.4.8;
-
 /// @title Time-locked vault of tokens allocated to Storj after variable amount of days
 /// based on Lunyr Token
 
 import 'zeppelin/contracts/token/StandardToken.sol';
 
-contract STORJVault is SafeMath {
-
-    StandardToken storjToken;
+contract TimeVault is SafeMath, StandardToken {
     address owner;
+    StandardToken storjToken;
     uint256 unlockedAtTime;
-    uint256 public constant timeLocked;
-
-
+    uint256 public timeLocked;
 
     /// @notice Constructor function sets the Storj Multisig address and
     /// total number of locked tokens to transfer
-    function STORJVault(address _owner, uint256 _timeLocked) internal {
+    function TimeVault(address _owner, uint256 _timeLocked, address _token) {
         if (_owner == 0x0) throw;
+        storjToken = StandardToken(_token);
         timeLocked = _timeLocked;
-        storjToken = StandardToken(msg.sender);
         owner = _owner;
         // variable time locked in seconds since epoch
         unlockedAtTime = safeAdd(block.timestamp, timeLocked);
@@ -30,7 +25,7 @@ contract STORJVault is SafeMath {
         // Wait your turn!
         if (block.timestamp < unlockedAtTime) throw;
         // Will fail if allocation (and therefore toTransfer) is 0.
-        if (!storjToken.transfer(storjMultisig, storjToken.balanceOf(this))) throw;
+        if(!storjToken.transfer(owner, storjToken.balanceOf(this))) throw;
     }
 
     // disallow ether payment
